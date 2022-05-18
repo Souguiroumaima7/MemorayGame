32463e8f18292ce0a345a0335065e0d12598d596
Souguir Oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2022-05-18 17:10:25 +0100

    myapp without modification

---
+++ memoryGame/src/test/kotlin/com/example/memoryGame/MemoryGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.memoryGame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemoryGameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
